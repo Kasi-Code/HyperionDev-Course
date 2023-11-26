4953fa277f83b34226815e3defd7c229131e4bec
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2023-11-26 15:03:44 +0000

    T09 tasks submitted

--- T09/ Task/errors.py
+++ T09/ Task/errors.py
@@ -2,23 +2,41 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-print "Welcome to the error program"
-    print "\n"
+# print "Welcome to the error program"
+print("Welcome to the error program") # FIXED: Syntax Error - missing parentheses.
+
+    # print "\n"
+print("\n") # FIXED: Syntax Error - unexpected indentation and missing parentheses.
 
     # Variables declaring the user's age, casting the str to an int, and printing the result
-    age_Str == "24 years old" 
-    age = int(age_Str) 
-    print("I'm" + age + "years old.")
+    # age_Str == "24 years old." 
+age_Str = "24" # FIXED: Syntax Error; Logical Error - unexpected indentation. Delete "years old." from string. Additionally, incorrectly used of comparsion "==" operator for "=" to assinged the variable. 
+
+    # age = int(age_Str)
+age = int(age_Str) # FIXED: Syntax Error - unexpected indentation.
+
+    # print("I'm" + age + "years old.")
+print("I'm " + str(age) + " years old.") # FIXED: Syntax Error; Runtime Error; Logical Error - unexpected indentation. Convert integer to string with str(). Need space " " in string beetween the age to correctly pritnt the sentence.  
+
 
     # Variables declaring additional years and printing the total years of age
-    years_from_now = "3"
-    total_years = age + years_from_now
+    # years_from_now = "3"
+years_from_now = 3 # FIXED: Syntax Error; Runtime Error - unexpected indentation. Changed variable from string to integer.
 
-print "The total number of years:" + "answer_years"
+    # total_years = age + years_from_now
+total_years = age + years_from_now # FIXED: Syntax Error -  unexpected indentation.
+
+# print "The total number of years:" + "answer_years"
+print("The total number of years: " + str(total_years)) # FIXED: Syntax Error; Runtime Error; Logical Error - missing parentheses. Changed sting to variable and convert it from integer to string with str().
+
 
 # Variable to calculate the total amount of months from the total amount of years and printing the result
-total_months = total * 12
-print "In 3 years and 6 months, I'll be " + total_months + " months old"
+# total_months = total * 12
+total_months = (total_years * 12) + 6 # FIXED: Runtime Error; Syntax Error - correctly name the chosen variable for the year. Needed extra six months extra.
+
+# print "In 3 years and 6 months, I'll be " + total_months + " months old"
+print("In 3 years and 6 months, I'll be " + str(total_months) + " months old.") # FIXED: Syntax Error; Runtime Error; Logical Error - missing parentheses. Changed sting to variable and convert it from integer to string with str().
 
 #HINT, 330 months is the correct answer
+
 
--- T09/ Task/errors2.py
+++ T09/ Task/errors2.py
@@ -2,11 +2,14 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-animal = Lion
+# animal = Lion
+animal = "Lion" # FIXED: Runtime Error; - missing quotations for the string.
 animal_type = "cub"
 number_of_teeth = 16
 
-full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+# full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+full_spec = f"This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth." # FIXED: Logical error - missing "f" in front of the quotation when embedding expressions in F-Strings. 
 
-print full_spec
+# print full_spec
+print(full_spec) # FIXED: Syntax Error; - missing parentheses.
 
