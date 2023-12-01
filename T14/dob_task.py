1788a3f83ca920429d99982db4dc0a677361ef1f
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2023-12-01 16:55:42 +0000

    Submitted T14

--- T14/dob_task.py
+++ T14/dob_task.py
@@ -2,12 +2,15 @@
 Practical Task
 ● Create a new Python file in the Dropbox folder for this task, and call it dob_task.py.
 ● In your Python file, write a program that reads the data from the text file provided (DOB.txt) and prints it out in two different sections in the format displayed below:
+
 Name
-Orville Wright Rogelio Holloway Marjorie Figueroa ... etc.
+Orville Wright 
+Rogelio Holloway 
+Marjorie Figueroa ... etc.
+
 Birthdate
 21 July 1988
 13 September 1988
- 
 9 October 1988 ... etc.
 
 """
