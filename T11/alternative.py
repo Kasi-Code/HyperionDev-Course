02ee44f9287ba0a28ae601315479ed0f6f69f206
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2023-12-10 15:49:04 +0000

    T11 fixed and re-submit

--- T11/alternative.py
+++ T11/alternative.py
@@ -21,26 +21,32 @@ Tip: Using the split() and join() functions will help you here.
 # Strings given.
 
 string = "Hello World"
-string_1 = "I am learning to code"
+string_1 = "I am learning to code"      
 
-# A variable of empty list is wating for the (while) loop to store the words in it.
+# The (while) loop will store the words/characters in the empty variables.
 # The string will get "split" into words.
-# The (while) loop will iterate through - depending on the index of the words.
-# Inside, the (if) statement will append each alternative word lower and upper case.
+# The (while) loop will iterate through - depending on the index of the words/char.
+# Inside, the (if) statement will append each alternative words/char as lower and upper case.
+# Finally, the (join) statement will stitch the list into a string sentence.
+
+str_list = []
+characters = string
+i = 0
+while i < len(characters):
+    if i % 2 == 1:
+        str_list.append(characters[i].lower())
+    else:
+        str_list.append(characters[i].upper())
+    i += 1
+print("".join(str_list))
 
 list_str = []
-
-words = string.split(" ")
-
+words = string_1.split(" ")
 i = 0
-
 while i < len(words):
-      
-      if i % 2 == 0:
-            list_str.append(words[i].lower())
-      else:
-            list_str.append(words[i].upper())
-
-      i += 1
-
+    if i % 2 == 0:
+        list_str.append(words[i].lower())
+    else:
+        list_str.append(words[i].upper())
+    i += 1
 print(" ".join(list_str))
