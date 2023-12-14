fe8a631e515b37755685bf1a201f6c0aede02305
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2023-12-14 23:32:35 +0000

    T17: trying to re-arrange tasks index

--- T17/tasks/functions.py
+++ T17/tasks/functions.py
@@ -30,18 +30,14 @@ def arranged_task_index(task_index):
     
     # print(tasks_dic)
 
-    tasks = []
     for key, value in tasks_dic.items():
         if key > 0:
             tasks = value
             due_date = value[85:95]
             # print(f"{key}: {value}")
+            
             num_dic = {0: 123, 1: 5, 2: 6, 3: 75}
-
-            # Use sorted to create a list of (key, value) tuples sorted by the values
             sorted_items = sorted(num_dic.items(), key=lambda x: x[1])
-
-            # Create a new dictionary from the sorted list of tuples
             sorted_dic = dict(sorted_items)
 
             # print(sorted_dic)
