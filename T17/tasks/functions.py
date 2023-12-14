73a913b8c4a5336caf3f18fecf1494b344b15f4e
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2023-12-14 23:29:07 +0000

    T17: trying to fix the tasks order

--- T17/tasks/functions.py
+++ T17/tasks/functions.py
@@ -25,8 +25,29 @@ def reg_user(input_name):
 
 def arranged_task_index(task_index):
     tasks_dic = {}
-    # x = slice(1,)
     for i,t in enumerate(task_index):
-        tasks_dic = {i:t}
+        tasks_dic.update({i:t}) 
     
-        print(tasks_dic(0))
+    # print(tasks_dic)
+
+    tasks = []
+    for key, value in tasks_dic.items():
+        if key > 0:
+            tasks = value
+            due_date = value[85:95]
+            # print(f"{key}: {value}")
+            num_dic = {0: 123, 1: 5, 2: 6, 3: 75}
+
+            # Use sorted to create a list of (key, value) tuples sorted by the values
+            sorted_items = sorted(num_dic.items(), key=lambda x: x[1])
+
+            # Create a new dictionary from the sorted list of tuples
+            sorted_dic = dict(sorted_items)
+
+            # print(sorted_dic)
+
+            print(f"{tasks}")
+
+    # filtered_dict = {key: value for key, value in tasks_dic.items() if key > 0}
+
+    # print(filtered_dict)
